2f6c12ea9a6256e0445f5c54da82dec526254a65
Fran Sarciat <fransarciat@gmail.com>
2017-02-13 17:38:18 +0100

    Css Pro ultimate version Beta de la alpha

--- FlightManagementDesktop/DashBoard.Designer.cs
+++ FlightManagementDesktop/DashBoard.Designer.cs
@@ -47,16 +47,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Font = new System.Drawing.Font("Arial Unicode MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Location = new System.Drawing.Point(46, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.Size = new System.Drawing.Size(79, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "Customer";
             // 
             // DDCustomer
             // 
             this.DDCustomer.FormattingEnabled = true;
-            this.DDCustomer.Location = new System.Drawing.Point(13, 30);
+            this.DDCustomer.Location = new System.Drawing.Point(12, 37);
             this.DDCustomer.Name = "DDCustomer";
             this.DDCustomer.Size = new System.Drawing.Size(146, 21);
             this.DDCustomer.TabIndex = 1;
@@ -64,16 +66,18 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 58);
+            this.label2.Font = new System.Drawing.Font("Arial Unicode MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label2.Location = new System.Drawing.Point(239, 13);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(53, 21);
             this.label2.TabIndex = 2;
             this.label2.Text = "ForTo";
             // 
             // DDFT
             // 
             this.DDFT.FormattingEnabled = true;
-            this.DDFT.Location = new System.Drawing.Point(13, 75);
+            this.DDFT.Location = new System.Drawing.Point(191, 37);
             this.DDFT.Name = "DDFT";
             this.DDFT.Size = new System.Drawing.Size(146, 21);
             this.DDFT.TabIndex = 3;
@@ -82,15 +86,17 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 103);
+            this.label3.Font = new System.Drawing.Font("Arial Unicode MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label3.Location = new System.Drawing.Point(44, 72);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.Size = new System.Drawing.Size(81, 21);
             this.label3.TabIndex = 4;
             this.label3.Text = "Departure";
             // 
             // TextBoxDeparture
             // 
-            this.TextBoxDeparture.Location = new System.Drawing.Point(13, 120);
+            this.TextBoxDeparture.Location = new System.Drawing.Point(12, 96);
             this.TextBoxDeparture.Name = "TextBoxDeparture";
             this.TextBoxDeparture.Size = new System.Drawing.Size(146, 20);
             this.TextBoxDeparture.TabIndex = 5;
@@ -98,15 +104,17 @@
             // Label4
             // 
             this.Label4.AutoSize = true;
-            this.Label4.Location = new System.Drawing.Point(13, 147);
+            this.Label4.Font = new System.Drawing.Font("Arial Unicode MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label4.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.Label4.Location = new System.Drawing.Point(232, 72);
             this.Label4.Name = "Label4";
-            this.Label4.Size = new System.Drawing.Size(40, 13);
+            this.Label4.Size = new System.Drawing.Size(60, 21);
             this.Label4.TabIndex = 6;
             this.Label4.Text = "Arrived";
             // 
             // TextBoxArrived
             // 
-            this.TextBoxArrived.Location = new System.Drawing.Point(13, 164);
+            this.TextBoxArrived.Location = new System.Drawing.Point(191, 96);
             this.TextBoxArrived.Name = "TextBoxArrived";
             this.TextBoxArrived.Size = new System.Drawing.Size(146, 20);
             this.TextBoxArrived.TabIndex = 7;
@@ -114,15 +122,17 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(13, 191);
+            this.label5.Font = new System.Drawing.Font("Arial Unicode MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label5.Location = new System.Drawing.Point(46, 135);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.Size = new System.Drawing.Size(79, 21);
             this.label5.TabIndex = 8;
             this.label5.Text = "Company";
             // 
             // TextBoxCompany
             // 
-            this.TextBoxCompany.Location = new System.Drawing.Point(13, 208);
+            this.TextBoxCompany.Location = new System.Drawing.Point(12, 159);
             this.TextBoxCompany.Name = "TextBoxCompany";
             this.TextBoxCompany.Size = new System.Drawing.Size(146, 20);
             this.TextBoxCompany.TabIndex = 9;
@@ -130,28 +140,33 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(13, 235);
+            this.label6.Font = new System.Drawing.Font("Arial Unicode MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label6.Location = new System.Drawing.Point(243, 135);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.Size = new System.Drawing.Size(43, 21);
             this.label6.TabIndex = 10;
             this.label6.Text = "Seat";
             // 
             // DDRow
             // 
             this.DDRow.FormattingEnabled = true;
-            this.DDRow.Location = new System.Drawing.Point(13, 252);
+            this.DDRow.Location = new System.Drawing.Point(191, 159);
             this.DDRow.Name = "DDRow";
             this.DDRow.Size = new System.Drawing.Size(146, 21);
             this.DDRow.TabIndex = 11;
             // 
             // ButtonGetValues
             // 
-            this.ButtonGetValues.Location = new System.Drawing.Point(13, 279);
+            this.ButtonGetValues.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ButtonGetValues.Font = new System.Drawing.Font("Arial Unicode MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonGetValues.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ButtonGetValues.Location = new System.Drawing.Point(106, 223);
             this.ButtonGetValues.Name = "ButtonGetValues";
-            this.ButtonGetValues.Size = new System.Drawing.Size(75, 23);
+            this.ButtonGetValues.Size = new System.Drawing.Size(133, 39);
             this.ButtonGetValues.TabIndex = 12;
             this.ButtonGetValues.Text = "Buy";
-            this.ButtonGetValues.UseVisualStyleBackColor = true;
+            this.ButtonGetValues.UseVisualStyleBackColor = false;
             this.ButtonGetValues.Click += new System.EventHandler(this.ButtonGetValues_Click);
             // 
             // LabelResult
@@ -166,7 +181,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(281, 353);
+            this.ClientSize = new System.Drawing.Size(348, 301);
             this.Controls.Add(this.LabelResult);
             this.Controls.Add(this.ButtonGetValues);
             this.Controls.Add(this.DDRow);
@@ -182,7 +197,7 @@
             this.Controls.Add(this.DDCustomer);
             this.Controls.Add(this.label1);
             this.Name = "DashBoard";
-            this.Text = "Form1";
+            this.Text = "Buy your ticket";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
